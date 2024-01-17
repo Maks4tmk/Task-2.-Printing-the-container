43118e56df1d2ccc35505e4e9f8ac505c20308c9
Maksim <maks4tmk5423@yandex.ru>
2024-01-17 23:28:15 +0300

    initial commit

---
+++ Задача 2. Печать контейнера/Задача 2. Печать контейнера.cpp
@@ -0,0 +1,28 @@
+﻿// Задача 2. Печать контейнера.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <set>
+#include <list>
+#include <vector>
+
+template<class T>
+void print_container(T& container) {
+    for (const auto i : container) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set); // four one three two. 
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list); // one, two, three, four
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector); // one, two, three, four
+}
+
